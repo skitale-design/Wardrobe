b683c4aa9ce5ba827c45dbf53ff150749f7de584
skitale <skitale85@gmail.com>
2017-08-11 22:13:07 +0300

    test

--- spec/cloth_spec.rb
+++ spec/cloth_spec.rb
@@ -5,7 +5,6 @@ describe 'Cloth' do
 
   it 'Read from test.txt file' do
 
-
     expect(test_cloth.name).to eq 'Тестовое название одежды'
     expect(test_cloth.type).to eq 'Тестовый тип'
     expect(test_cloth.temperature_range).to eq  -10..10
@@ -17,6 +16,4 @@ describe 'Cloth' do
     expect(test_cloth.fits?(-15)).to eq false
 
   end
-
-
 end
